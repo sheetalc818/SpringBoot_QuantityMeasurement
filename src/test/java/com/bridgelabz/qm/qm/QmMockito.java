72c3077a4ca6030e65d25daf19e417c721f846aa
sheetalc818 <sheetalc818@gmail.com>
2020-04-14 22:59:36 +0530

    Added Mockito Test for the api to get all the subUnits of units

--- src/test/java/com/bridgelabz/qm/qm/QmMockito.java
+++ src/test/java/com/bridgelabz/qm/qm/QmMockito.java
@@ -1,17 +1,26 @@
 package com.bridgelabz.qm.qm;
 
 import com.bridgelabz.qm.qm.controller.QmController;
+import com.bridgelabz.qm.qm.enumeration.Unit;
+import com.bridgelabz.qm.qm.enumeration.UnitType;
+import com.bridgelabz.qm.qm.service.IQmService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -21,13 +30,29 @@ public class QmMockito {
     @Autowired
     public MockMvc mockMvc;
 
+    @MockBean
+    private IQmService qmService;
+
+
     @Test
     public void getAllMainUnitsAPI() throws Exception
     {
-        mockMvc.perform( MockMvcRequestBuilders
+        mockMvc.perform(MockMvcRequestBuilders
                 .get("/quantityMeasurement/unit/type")
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void givenUnit_ShouldReturnUnitSubType() throws Exception {
+        List<UnitType> unitTypeList = Arrays.stream(UnitType.values()).filter(unitType -> unitType.unit.equals(Unit.VOLUME)).collect(Collectors.toList());
+        given(qmService.getUnitType(Unit.VOLUME)).willReturn(unitTypeList);
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/quantityMeasurement/unit/subtype?unit={unit}", Unit.VOLUME)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(String.valueOf(unitTypeList)));
     }
 }
